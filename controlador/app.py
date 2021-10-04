10c6993124e5cdf327bb4ad0bb4fde12dc092bbf
robertzinzun <robertzinzun@gmail.com>
2021-10-04 12:02:00 -0500

    login
    Creacion de los elementos para el login

--- controlador/app.py
+++ controlador/app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 
 app=Flask(__name__,template_folder='../vista')
 
@@ -12,5 +12,13 @@ def listarProductos():
 
     return render_template('productos/listado.html')
 
+@app.route('/usuarios/login',methods=['post'])
+def login():
+    email=request.form['email']
+    return 'Validando la cuenta de usuario:'+email
+
+@app.route('/usuarios/ingresar')
+def ingresar():
+    return render_template('usuarios/login.html')
 if __name__=='__main__':
     app.run(debug=True)
