33a4626c5e405b34567c9162ebd6f1d364a3db0c
robertzinzun <robertzinzun@gmail.com>
2021-10-06 14:04:05 -0500

    Definiendo la navegación
    Navegación de los recursos de categorias

--- controlador/app.py
+++ controlador/app.py
@@ -20,5 +20,19 @@ def login():
 @app.route('/usuarios/ingresar')
 def ingresar():
     return render_template('usuarios/login.html')
+#Seccion para las rutas de categorias
+@app.route('/categorias')
+def categorias():
+    return render_template('categorias/consulta.html')
+
+@app.route('/categorias/nuevo')
+def nuevaCategoria():
+    return render_template('categorias/nuevo.html')
+
+@app.route('/categorias/editar')
+def editarCategoria():
+    return render_template('categorias/editar.html')
+
+# fin de la seccion de categorias
 if __name__=='__main__':
     app.run(debug=True)
